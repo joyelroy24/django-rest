13226693af4192b9da95b2be4fd2787c603a7567
joyel <joyelroy24@gmail.com>
2024-01-01 14:33:36 +0530

    rest view and response

--- backend/home/api/views.py
+++ backend/home/api/views.py
@@ -1,30 +1,38 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
-
+from products.models import Product
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
-
+from products.serializers import ProductSerializer
+@api_view(['GET'])
 def api_home(request,*args,**kwargs):
     data={}
-    print("body below")
-    print(request.body)
-    try:
-        print(type(request.body)) #class byte
-        # data=request.body.decode('utf-8')
-        # print(data)
-        # print(type(data))
-        data=json.loads(request.body) #convert to dictionary format
-        print("Loadssss")
-        print(data)
-        print(type(data))  
-        
-    except json.JSONDecodeError as e:
-        print(e)
-
-    print(print(type(request.headers)))
-    print(dict(request.headers))
-    data['header']=dict(request.headers)
-    data['content_type']=request.content_type
+    model_data=Product.objects.all().order_by("?").first()
+    if model_data:
+        data=model_to_dict(model_data,fields=['id','title','sale_price'])
     
-    data['get_parameter']=request.GET
-    return JsonResponse(data)
+    # print("body below")
+    # print(request.body)
+    # try:
+    #     print(type(request.body)) #class byte
+    #     # data=request.body.decode('utf-8')
+    #     # print(data)
+    #     # print(type(data))
+    #     data=json.loads(request.body) #convert to dictionary format
+    #     print("Loadssss")
+    #     print(data)
+    #     print(type(data))  
+        
+    # except json.JSONDecodeError as e:
+    #     print(e)
+
+    # print(print(type(request.headers)))
+    # print(dict(request.headers))
+    # data['header']=dict(request.headers)
+    # data['content_type']=request.content_type
+    
+    # data['get_parameter']=request.GET
+    return Response(data)
--- backend/home/products/models.py
+++ backend/home/products/models.py
@@ -9,3 +9,7 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return self.title
+    
+    @property
+    def sale_price(self):
+        return "%.2f"%(float(self.price)*0.10)
