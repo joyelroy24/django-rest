ee60ad8ccc185af85a07570f3ad31e92698a89ad
joyel <joyelroy24@gmail.com>
2024-01-03 12:10:39 +0530

    Generic Create,Detail view

--- backend/home/products/views.py
+++ backend/home/products/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    # queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+    #lookup_field='pk'
+
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+
+    def perform_create(self, serializer):
+        title=serializer.validated_data.get('title')
+        serializer.save()
+
+
+ProductCreateapiview=ProductCreateAPIView.as_view()#avoid as view on urls.py
+
+
+class ProductListAPIView(generics.ListAPIView):
+    # not used this this is implemented same on create
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+
+    def perform_create(self, serializer):
+        title=serializer.validated_data.get('title')
+        serializer.save()
---
+++ client/detail.py
@@ -0,0 +1,11 @@
+import requests
+
+
+endpoint = "http://localhost:8000/api/products/1" 
+
+get_response = requests.get(endpoint) # HTTP Request
+
+print(get_response.json())
+# print(get_response.json())
+
+# print(get_response.status_code)
